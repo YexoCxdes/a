cfaeacdf0a5c2fe522a460d2ac958d73740c72be
YexoCxdes <149716746+YexoCxdes@users.noreply.github.com>
2025-03-07 18:53:27 +0000

    Add files via upload

---
+++ src/main2605.tsx
@@ -0,0 +1,11 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/@fs/DATA/Node/DiscordActivitiesTracker/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=bee6fbf6"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_reactDom_client from "/@fs/DATA/Node/DiscordActivitiesTracker/node_modules/.vite/deps/react-dom_client.js?v=bee6fbf6"; const createRoot = __vite__cjsImport1_reactDom_client["createRoot"];
+import App from "/src/App.tsx?v=wvN5fF7FwumY-rTypeDBG";
+import "/src/index.css?v=wvN5fF7FwumY-rTypeDBG";
+createRoot(document.getElementById("root")).render(/* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+  fileName: "/DATA/Node/DiscordActivitiesTracker/client/src/main.tsx?v=wvN5fF7FwumY-rTypeDBG",
+  lineNumber: 5,
+  columnNumber: 53
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBSW9EO0FBSnBELFNBQVNBLGtCQUFrQjtBQUMzQixPQUFPQyxTQUFTO0FBQ2hCLE9BQU87QUFFUEQsV0FBV0UsU0FBU0MsZUFBZSxNQUFNLENBQUUsRUFBRUMsT0FBTyx1QkFBQyxTQUFEO0FBQUE7QUFBQTtBQUFBO0FBQUEsT0FBSSxDQUFHIiwibmFtZXMiOlsiY3JlYXRlUm9vdCIsIkFwcCIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiLCJyZW5kZXIiXSwiaWdub3JlTGlzdCI6W10sInNvdXJjZXMiOlsibWFpbi50c3giXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgY3JlYXRlUm9vdCB9IGZyb20gXCJyZWFjdC1kb20vY2xpZW50XCI7XG5pbXBvcnQgQXBwIGZyb20gXCIuL0FwcFwiO1xuaW1wb3J0IFwiLi9pbmRleC5jc3NcIjtcblxuY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZChcInJvb3RcIikhKS5yZW5kZXIoPEFwcCAvPik7XG4iXSwiZmlsZSI6Ii9EQVRBL05vZGUvRGlzY29yZEFjdGl2aXRpZXNUcmFja2VyL2NsaWVudC9zcmMvbWFpbi50c3gifQ==
